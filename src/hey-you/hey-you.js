cf82c3b6da2f9fec4feb7277a6e3ca9d7a80f9bd
Damandeep Singh <mail@damandeepsingh.com>
2018-06-14 00:39:16 +0530

    Drawer

--- src/hey-you/hey-you.js
+++ src/hey-you/hey-you.js
@@ -1,4 +1,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/app-layout/app-layout.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/iron-icons/iron-icons.js';
 
 /**
  * @customElement
@@ -8,12 +11,67 @@ class HeyYou extends PolymerElement {
   static get template() {
     return html`
       <style>
-        :host {
-          display: block;
-        }
+      :host {
+        --app-primary-color: #4285f4;
+        --app-secondary-color: black;
+
+        display: block;
+      }
+
+      app-drawer-layout:not([narrow]) [drawer-toggle] {
+        display: none;
+      }
+
+      app-header {
+        color: #fff;
+        background-color: var(--app-primary-color);
+      }
+
+      app-header paper-icon-button {
+        --paper-icon-button-ink-color: white;
+      }
+
+      .drawer-list {
+        margin: 0 20px;
+      }
+
+      .drawer-list a {
+        display: block;
+        padding: 0 16px;
+        text-decoration: none;
+        color: var(--app-secondary-color);
+        line-height: 40px;
+      }
+
+      .drawer-list a.iron-selected {
+        color: black;
+        font-weight: bold;
+      }
       </style>
-      <h2>Hello [[prop1]]!</h2>
-    `;
+
+
+      <app-drawer-layout>
+        <app-drawer slot="drawer">
+          drawer-content
+        </app-drawer>
+        <app-header-layout>
+
+          <app-header reveals>
+            <app-toolbar>
+              <paper-icon-button icon="menu"  drawer-toggle></paper-icon-button>
+              <div main-title>My app</div>
+              <paper-icon-button icon="delete"></paper-icon-button>
+              <paper-icon-button icon="search"></paper-icon-button>
+              <paper-icon-button icon="close"></paper-icon-button>
+              <paper-progress value="10" indeterminate bottom-item></paper-progress>
+            </app-toolbar>
+          </app-header>
+
+          main content
+
+        </app-header-layout>
+      </app-drawer-layout>
+          `;
   }
   static get properties() {
     return {
@@ -23,6 +81,8 @@ class HeyYou extends PolymerElement {
       }
     };
   }
+
+
 }
 
 window.customElements.define('hey-you', HeyYou);
